a0cdf38ab9425d3a42772a47d53ddbb99e27e90c
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-01-29 00:23:48 -0800

    fixed Gamemanager

--- GameManager.js
+++ GameManager.js
@@ -1,4 +1,4 @@
-import { SoundManager } from './SoundManager.js';
+import SoundManager from './SoundManager.js';
 export default class GameManager {
   constructor(ball, paddle, bricks, score, lives, canvas) {
     this.ball = ball;
