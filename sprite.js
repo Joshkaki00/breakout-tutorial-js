dcd3c580f900a445107e3362e74d2a42711b17df
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-02-06 14:15:04 -0800

    made sprite js

---
+++ sprite.js
@@ -0,0 +1,17 @@
+export default class Sprite {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
